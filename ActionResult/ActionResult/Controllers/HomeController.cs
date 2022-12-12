41331c461fa7d2dc3fadadde5c9fac55b5ac3ca0
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2022-12-12 14:01:12 -0300

    C# Code
    C# Code for download File

---
+++ ActionResult/ActionResult/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ActionResult.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionResult.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            //return Json("{'nome': 'Eduardo Olimpio'}");
+
+            var fileBytes = System.IO.File.ReadAllBytes(@"d:\arquivo.txt");
+            var fileName = "ola.txt";
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+            //return Content("Qualquer coisa");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
